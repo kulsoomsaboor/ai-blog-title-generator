3e017d1cd04dfa75f5a3b30c9f07ce345d399db6
kulsoomsaboor <iamkulsoom6@gmail.com>
2025-05-19 17:09:25 +0500

    Add /generate-titles POST route
    - basic post route created
    - integrated with openai client for title generation

--- app/routes/generator.py
+++ app/routes/generator.py
@@ -1,7 +1,25 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from app.services.openai_client import generate_titles
+from fastapi import HTTPException
+
+
 
 router = APIRouter()
+
+class BlogContent(BaseModel):
+    prompt: str
 
 @router.get("/")
 def read_root():
     return {"message": "Blog Title Generator API"}
+
+@router.post("/generate-titles")
+async def generate_reponse(blog: BlogContent):
+    try:
+        titles = await generate_titles(blog.prompt)
+        return {"titles": titles}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    
--- app/services/openai_client.py
+++ app/services/openai_client.py
@@ -1,5 +1,20 @@
 from dotenv import load_dotenv
 import os
 
+import openai 
+
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+async def generate_titles(prompt: str) -> list:
+    response = openai.responses.create(
+        model="gpt-4.1",
+        input="Write a one-sentence bedtime story about a unicorn."
+    )
+ 
+    return print(response.output_text)
+
+
+
+
