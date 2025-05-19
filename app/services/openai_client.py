0e1bddf731de64d0ce4a5afd4667868ea2344dc5
kulsoomsaboor <iamkulsoom6@gmail.com>
2025-05-19 18:11:35 +0500

    Update openai_client
    - added default prompt (blog content)
    - changed Responses API to Chat Completion API in generate_titles method for simplicity

--- app/services/openai_client.py
+++ app/services/openai_client.py
@@ -1,19 +1,30 @@
 from dotenv import load_dotenv
 import os
 
-import openai 
+from openai import OpenAI
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI()
 
 
-async def generate_titles(prompt: str) -> list:
-    response = openai.responses.create(
+
+async def generate_titles( 
+        prompt_content:
+        str = "Healthy eating can transform your energy levels, mood, and focus. Here’s how to start " ) -> list:
+    response = client.chat.completions.create(
         model="gpt-4.1",
-        input="Write a one-sentence bedtime story about a unicorn."
+        temperature=0.7,
+        messages=[
+            {"role": "user", 
+             "content": f"Generate 3 catchy titles for a blog post based on this content: \n {prompt_content}"}
+        ]
     )
- 
-    return print(response.output_text)
+    
+    return response.choices[0].message.content.split('\n')
+
+
 
 
 
