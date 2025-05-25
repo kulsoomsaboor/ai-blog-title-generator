22403b4afaac7f50ef7c244e3bcccd5d1ea0011b
kulsoomsaboor <iamkulsoom6@gmail.com>
2025-05-25 17:43:58 +0500

    Set up CORS to enable API access from HTML form
    - added middleware to allow frontend requests

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from app.routes import generator
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],  # frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP methods: POST, GET, etc.
+    allow_headers=["*"],  # allow all headers
+)
+
 app.include_router(generator.router)
+
+
+
+
